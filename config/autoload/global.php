d77356a84a6b71cea1761c5b291d433631282b88
john meah <johnmeah@memreas.com>
2013-10-25 00:36:03 -0400

    25-OCT-2013 updated memreasintdb database address

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -47,7 +47,7 @@ return array(
                                                     'bit' => 'string'),
                 'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
                 'params' => array(
-                    'host' => 'aa15nf7gzm5gbt3.co0fw2snbu92.us-east-1.rds.amazonaws.com',
+                    'host' => 'aa1qhjij4wk4yji.co0fw2snbu92.us-east-1.rds.amazonaws.com',
                     'port' => '3306',
                     'dbname' => 'memreasintdb',
                     'user'     => 'memreasdbuser',
