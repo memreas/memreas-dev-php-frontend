29c9b8dac6c45ef9a34e3b76971177ecc82de14d
Tran Tuan <divand553@gmail.com>
2013-11-06 23:20:46 +0700

    Update gallery page asset files

--- app/memreas/js/fetchLocalMedia.js
+++ app/memreas/js/fetchLocalMedia.js
@@ -1,5 +1,5 @@
   function readFilesAndDisplayPreview(files) {
-alert('readFilesAndDisplayPreview fired...');
+//alert('readFilesAndDisplayPreview fired...');
     // Loop through the FileList and render image files as thumbnails.
     for (var i = 0, f; f = files[i]; i++) {
       // Only process image files.
@@ -11,13 +11,16 @@ alert('readFilesAndDisplayPreview fired...');
       reader.onload = (function(theFile) {
         return function(e) {
           // Render thumbnail.
-          var link = "<a href='#'><img src='" + e.target.result + "' alt='" + e.target.result + "' /></a>"; 
-		  $("#content_1 .mCSB_container").append(link);
+          //var link = "<a href='#'><img src='" + e.target.result + "' alt='" + e.target.result + "' /></a>";
+          var link = '<img src="' + e.target.result + '"/>';
+		  //$("#content_1 .mCSB_container").append(link);
+          $(".user-resources").append (link);
+          $(".scrollClass .mCSB_container").append ('<li><a class="class="swipebox" href="' + e.target.result + '">' + link + '</a></li>');
         };
 
       })(f);
       // Read in the image file as a data URL.
-      reader.readAsDataURL(f);
+      files_read = reader.readAsDataURL(f);
     }
   }
 
@@ -26,4 +29,13 @@ alert('readFilesAndDisplayPreview fired...');
     readFilesAndDisplayPreview(files);
   }
 
-//  document.getElementById('dir').addEventListener('change', handleFileSelect, false);
+  $(function(){
+    //document.getElementById('dir').addEventListener('change', handleFileSelect, false);
+    $("#dir").change (function(e){
+        $("#tab-content style, .fotorama--hidden").remove();
+        $(".user-resources").attr ('class', 'user-resources');
+        $(".user-resources, .scrollClass .mCSB_container").html('');
+        handleFileSelect(e);
+        setTimeout(function(){ $(".user-resources").fotorama(); $(".browse-file").hide(); }, 1000);
+    });
+  });
