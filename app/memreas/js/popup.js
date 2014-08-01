48da5e68b3a7be947c2931baed3c06dee40fe99d
Samiullah Khan <samiulah.khan.1989@gmail.com>
2014-08-01 14:51:14 +0500

    MFE-357 is ready for testing
    Here I've centered the new card. Secondly I've added a feature where the
    popup closes when you click on overlay.

--- app/memreas/js/popup.js
+++ app/memreas/js/popup.js
@@ -10,17 +10,22 @@ var popupStatus = 0;
 //loading popup with jQuery magic!
 function loadPopup(id){
 	//loads popup only if it is disabled
-	if(popupStatus==0){
+	if(popupStatus===0){
 		$("#bg"+id).css({
 			"opacity": "0.7"
 		});
 		$("#bg"+id).fadeIn("slow");
+		$("body,html").animate({
+			scrollTop:$('body').offset().top
+		},700);
 		$("#"+id).fadeIn("slow");
 		popupStatus = 1;
 	}
 }
 
 //disabling popup with jQuery magic!
+// $("#bgpopupaccountaddcard").on('click', disablePopup(id));
+
 function disablePopup(id){
 	//disables popup only if it is enabled
 	if(popupStatus==1){
@@ -35,16 +40,27 @@ function centerPopup(id){
 	$(".popups").fadeOut("slow");
 		$(".backgroundPopup").fadeOut("slow");
 	//request data for centering
-	var windowWidth = document.documentElement.clientWidth;
-	var windowHeight = document.documentElement.clientHeight;
-	var popupHeight = $("#"+id).height();
-	var popupWidth = $("#"+id).width();
+	// var windowWidth = document.documentElement.clientWidth;
+	// var windowHeight = document.documentElement.clientHeight;
+	var windowWidth = window.innerWidth;
+	var windowHeight = window.innerHeight;
+	var popupHeight = $("#"+id).outerHeight();
+	var popupWidth = $("#"+id).outerWidth();
 	//centering
 	$("#"+id).css({
 		"position": "absolute",
-		"top": windowHeight/2-popupHeight/2,
-		"left": windowWidth/2-popupWidth/2
+		// "top": windowHeight/2-popupHeight/2,
+		// "left": windowWidth/2-popupWidth/2
+		"top": (windowHeight-popupHeight)/2,
+		// "left": (windowWidth-popupWidth)/2
+		"left": "37%"
+
 	});
+	if(window.innerWidth < 991){
+		$("#"+id).css({
+			"left":"23%"
+		});	
+	}
 	//only need force for IE6
 
 	$("#bg"+id).css({
