24e6f68bde00d492a86de3ab1aaa3a586ec8175d
Samiullah Khan <samiulah.khan.1989@gmail.com>
2014-09-02 21:44:40 +0500

    Re-Worked on MFE-466

--- app/memreas/js/gallery.js
+++ app/memreas/js/gallery.js
@@ -69,7 +69,10 @@ var checkHasImage = false;
 jQuery.fetch_server_media = function (){
     $(".user-resources").remove();
     // Khan Changes
-    if(window.innerWidth > 1359 && window.innerHeight > 800){
+    if(window.innerHeight <= 660){
+        $("#tab-content #tab1").append('<div class="user-resources" data-click="false" data-swipe="true" data-ratio="800/725" data-max-width="100%" data-height="50%" data-allow-full-screen="true"  data-nav="thumbs"></div>');
+    }
+    else if(window.innerWidth > 1359 && window.innerHeight > 800){
         $("#tab-content #tab1").append('<div class="user-resources" data-click="false" data-swipe="true" data-ratio="800/725" data-max-width="100%" data-height="55%" data-allow-full-screen="true"  data-nav="thumbs"></div>');
     } else {
         $("#tab-content #tab1").append('<div class="user-resources" data-click="false" data-swipe="true" data-ratio="800/725" data-max-width="100%" data-allow-full-screen="true"  data-nav="thumbs"></div>');
