c685ad23d27f28f0d884e5a3902853024cefd0c5
Tran Tuan <tranit1209@gmail.com>
2014-09-09 00:41:31 +0700

    Correct media thumb url on share/memreas

--- app/deploy_test.php
+++ app/deploy_test.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "Deploy for frontend v1.2.3 - File size upload increased / remove upload debug code - ready for review";
+echo "Deploy for frontend v1.2.4 - Correct share/memreas list media - ready for review";
 
 ?>
