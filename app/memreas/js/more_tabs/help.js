cb506106304e621cf7ea969eb0babdfa5b19e47f
Tran Tuan <tranit1209@gmail.com>
2014-09-23 21:19:37 +0700

    Help page contents

---
+++ app/memreas/js/more_tabs/help.js
@@ -0,0 +1,17 @@
+$(function(){
+    $(".member-guidelines-head").one('click', function(){
+        $(".member-guidelines").mCustomScrollbar({scrollButtons:{enable:true }});
+    });
+
+    $(".privacy-policy-head").one("click", function(){
+        $(".privacy-policy").mCustomScrollbar({scrollButtons:{enable:true }});
+    });
+
+    $(".terms-service-head").one("click", function(){
+        $(".terms-service").mCustomScrollbar({scrollButtons:{enable:true }});
+    });
+
+    $(".dmca-policy-head").one("click", function(){
+        $(".dmca-policy").mCustomScrollbar({scrollButtons:{enable:true }});
+    });
+});
