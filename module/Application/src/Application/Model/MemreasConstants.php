254b3a3a2ea8aca6297a7bead7277b303640a9e3
john meah <johnmeah@memreas.com>
2014-10-04 13:16:19 -0400

    3-OCT-2014 matched constants in js and fe

--- module/Application/src/Application/Model/MemreasConstants.php
+++ module/Application/src/Application/Model/MemreasConstants.php
@@ -7,10 +7,10 @@ namespace Application\Model;
 
 class MemreasConstants {
 	
-	const MEMREAS_FE = "https://fe2.memreas.com";
+	const MEMREAS_FE = "https://fe.memreas.com";
 	const MEMREAS_WS = "https://memreasdev-wsg.memreas.com";
 	const MEMREAS_PAY = "https://memreasdev-pay.memreas.com";
-	const MEMREAS_ADS = false;
+	const MEMREAS_ADS = true;
     const S3_APPKEY = 'AKIAJMXGGG4BNFS42LZA';
     const S3_APPSEC = 'xQfYNvfT0Ar+Wm/Gc4m6aacPwdT5Ors9YHE/d38H';
 	const S3BUCKET = "memreasdevsec";
