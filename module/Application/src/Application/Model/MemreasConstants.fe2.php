6bf15a64233e5da0ba26d6baed8c1802f454cf58
john meah <johnmeah@memreas.com>
2014-10-20 11:12:43 -0400

    20-OCT-2014 update for basic auth

--- module/Application/src/Application/Model/MemreasConstants.fe2.php
+++ module/Application/src/Application/Model/MemreasConstants.fe2.php
@@ -5,7 +5,7 @@
 // ///////////////////////////////
 namespace Application\Model;
 
-class MemreasEnvConstants {
+class MemreasConstants {
 	
 	const MEMREAS_FE = "https://fe2.memreas.com";
 	const MEMREAS_WS = "https://memreasdev-wsg.memreas.com";
