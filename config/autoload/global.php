476211d6202367fc20a6c8dc826d37dac0773758
Pham Van Mong <isotemplates@gmail.com>
2014-10-28 14:47:52 +0700

    Im just changed 1 comment and testing commit

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -9,6 +9,7 @@
  * @NOTE: In practice, this file will typically be INCLUDED in your source
  * control, so do not include passwords or other sensitive information in this
  * file.
+ * I will try test on commit
  */
 
 return array(
