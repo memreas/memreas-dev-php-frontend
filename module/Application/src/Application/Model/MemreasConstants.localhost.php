e191401bdcffb6e40a35708d94cc7bc91cbe521e
john meah <johnmeah@memreas.com>
2014-11-01 02:38:50 -0400

    1-NOV-2014 added localhost config file

---
+++ module/Application/src/Application/Model/MemreasConstants.localhost.php
@@ -0,0 +1,32 @@
+<?php
+// ///////////////////////////////
+// Author: John Meah
+// Copyright memreas llc 2013
+// ///////////////////////////////
+namespace Application\Model;
+
+class MemreasConstants {
+	
+// 	const MEMREAS_FE = "https://fe.memreas.com";
+// 	const MEMREAS_WS = "https://memreasdev-wsu.memreas.com";
+// 	const MEMREAS_PAY = "https://memreasdev-pay.memreas.com";
+	const MEMREAS_FE = "http://memreas-dev-php-frontend.localhost/";
+	const MEMREAS_WS = "http://memreas-dev-php-ws.localhost/?XDEBUG_SESSION_START=ECLIPSE_DBGP";
+	const MEMREAS_PAY = "http://memreas-dev-php-stripe.localhost/";
+	const MEMREAS_ADS = true;
+    const CLOUDFRONT_DOWNLOAD_HOST = 'https://d3sisat5gdssl6.cloudfront.net/';
+    const S3_APPKEY = 'AKIAJMXGGG4BNFS42LZA';
+    const S3_APPSEC = 'xQfYNvfT0Ar+Wm/Gc4m6aacPwdT5Ors9YHE/d38H';
+	const S3BUCKET = "memreasdevsec";
+    const LISTNOTIFICATIONSPOLLTIME = 15000; //15s
+
+    //For file limit upload
+    const FREE_ACCOUNT_FILE_LIMIT = 100; // 100MB limited upload for free user
+    const PAID_ACCOUNT_FILE_LIMIT = 5000; // 5GB limited upload for paid user
+
+    //Enable / Disable payment tabs at more page
+    const PAYMENT_TAB_SUBSCRIPTION = true;
+    const PAYMENT_TAB_BUY_CREDIT = true;
+    const PAYMENT_TAB_SELL_MEDIA = true;
+
+}
