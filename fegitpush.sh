ae5ce7f1c035f6c542f7f1f9b4468296f74beee1
john meah <johnmeah@memreas.com>
2014-11-01 19:09:38 -0400

    1-NOV-2014 updated composer.phar - re-run php composer.phar self-update and php composer.phar update locally

--- fegitpush.sh
+++ fegitpush.sh
@@ -6,7 +6,6 @@
 echo -n "Enter the details of your deployment (i.e. 4-FEB-2014 Updating this script.) > "
 read comment
 echo "You entered $comment"
-#set -v verbose #echo on
 
 #copy fe settings to push to git...
 cp ./module/Application/src/Application/Model/MemreasConstants.fe.php ./module/Application/src/Application/Model/MemreasConstants.php
@@ -16,4 +15,7 @@ echo "Committing to git..."
 git add --all
 git commit -m "$comment"
 echo "Pushing to github..."
+set -v verbose #echo on
 git push
+
+cp module/Application/src/Application/Model/MemreasConstants.localhost.php module/Application/src/Application/Model/MemreasConstants.php
