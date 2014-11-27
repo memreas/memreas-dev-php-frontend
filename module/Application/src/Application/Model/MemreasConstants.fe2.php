827c392e236bfde1cc6a84f9ac260f9caa259f12
john meah <johnmeah@memreas.com>
2014-11-27 07:59:01 -0500

    27-NOV-2014 updated fe2 constants to turn off buy sell media

--- module/Application/src/Application/Model/MemreasConstants.fe2.php
+++ module/Application/src/Application/Model/MemreasConstants.fe2.php
@@ -10,7 +10,7 @@ class MemreasConstants {
 	const MEMREAS_FE = "https://fe2.memreas.com";
 	const MEMREAS_WS = "https://memreasdev-wsg.memreas.com";
 	const MEMREAS_PAY = "https://memreasdev-pay.memreas.com";
-	const MEMREAS_ADS = true;
+	const MEMREAS_ADS = false;
     const CLOUDFRONT_DOWNLOAD_HOST = 'https://d3sisat5gdssl6.cloudfront.net/';
     const S3_APPKEY = 'AKIAJMXGGG4BNFS42LZA';
     const S3_APPSEC = 'xQfYNvfT0Ar+Wm/Gc4m6aacPwdT5Ors9YHE/d38H';
@@ -21,9 +21,9 @@ class MemreasConstants {
 	const PAID_ACCOUNT_FILE_LIMIT = 5000; // 5GB limited upload for free user
 	
     //Enable / Disable payment tabs at more page
-    const PAYMENT_TAB_SUBSCRIPTION = true;
-    const PAYMENT_TAB_BUY_CREDIT = true;
-    const PAYMENT_TAB_SELL_MEDIA = true;
+    const PAYMENT_TAB_SUBSCRIPTION = false;
+    const PAYMENT_TAB_BUY_CREDIT = false;
+    const PAYMENT_TAB_SELL_MEDIA = false;
 
     const VERSION = '0.987';
 }
