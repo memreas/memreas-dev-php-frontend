2e643d316b48ffdff4553a1f946904f34f79f9b6
john meah <johnmeah@memreas.com>
2015-12-30 15:13:31 -0500

    .debugging search page

--- app/memreas/js/search.js
+++ app/memreas/js/search.js
@@ -302,8 +302,8 @@ function personalSearchLi(target, item) {
     //Prevent yourself listing
     if (('@' + $("input[name=username]").val()) != item.username){
         var photo = item.profile_photo;
-        alert(photo);
         photo = removeCdataCorrectLink(photo);
+        alert("user photo url -->"+photo);
         var name = $.trim(item.username);
         var op = '<li id="search-'+name.replace('@', '')+'"><figure class="pro-pics"><img src="'
                 + photo + '" alt=""></figure><div class="user-names">'
