d27dfbe24cb4d1b426caa12e2d5bf52b278f9ebd
shyamrockscs <shyam.rockscs@gmail.com>
2016-01-11 17:46:53 +0530

    11-1-2016 update

--- app/memreas/js/gallery.js
+++ app/memreas/js/gallery.js
@@ -318,7 +318,7 @@ jQuery.fetch_server_media = function () {
                                          + _media_thumbnail + '"  style="width:100%; height:400px;" preload="none" autoplay="">';
                                      source += '<source src="'
                                          + _media_url_web
-                                         + '" type="application/x-mpegURL">';
+                                         + '" type="vnd.apple.mpegURL">';
                                         									
                                          source += '</video>';
                                         
