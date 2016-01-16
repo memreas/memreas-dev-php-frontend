9fcfb25cd3b88b781cac7b17da78afad09745dfc
john meah <johnmeah@memreas.com>
2016-01-15 23:26:50 -0500

    .backing out proxy

--- module/Application/src/Application/Model/MemreasConstants.localhost.php
+++ module/Application/src/Application/Model/MemreasConstants.localhost.php
@@ -13,8 +13,8 @@ class MemreasConstants {
 	// const MEMREAS_PAY = "https://memreasdev-pay.memreas.com";
 	const MEMREAS_FE = "http://memreas-dev-php-frontend.localhost";
 	const MEMREAS_WS = "https://memreasdev-wsj.memreas.com";
-	const MEMREAS_PAY = "https://memreasdev-wsj.memreas.com/?action=";
-	//const MEMREAS_PAY = "https://memreasdev-pay.memreas.com";
+	//const MEMREAS_PAY = "https://memreasdev-wsj.memreas.com/?action=";
+	const MEMREAS_PAY = "https://memreasdev-pay.memreas.com";
 	const MEMREAS_ADS = false;
 	const MEMREAS_SELL_MEDIA = 1; // Enable or Disable selling media
 	const CLOUDFRONT_DOWNLOAD_HOST = 'https://d3sisat5gdssl6.cloudfront.net/';
