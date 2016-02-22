027c64545e4a2ca58a63520759dc2181abf43472
Tran IT <tranit1209@gmail.com>
2016-02-22 16:52:11 +0700

    Adjust last commit

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -59,7 +59,7 @@ class IndexController extends AbstractActionController {
 		/*
 		 * Fetch guzzle and post...
 		 */
-		$guzzle = new \GuzzleHttp\Client (['verify' => false]);
+		$guzzle = new \GuzzleHttp\Client ();
 		//Mlog::addone ( __CLASS__ . __METHOD__ . 'about to guzzle url+action+xml', MemreasConstants::MEMREAS_WS . $action . $xml );
 		try {
 			$response = $guzzle->post ( MemreasConstants::MEMREAS_WS, [ 
