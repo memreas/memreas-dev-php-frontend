0d564fd46651bae0fdbc9079284d2f2975ddb581
john meah <johnmeah@memreas.com>
2016-03-17 16:12:59 -0400

    .

--- prodfegitpush.sh
+++ prodfegitpush.sh
@@ -20,5 +20,5 @@ git push
 
 cp module/Application/src/Application/Model/MemreasConstants.localhost.php module/Application/src/Application/Model/MemreasConstants.php
 
-curl https://memreas-dev-deploy.memreas.com/prodpull_n_aws_push.php?repo=frontend
+curl https://memreas-dev-deploy.memreas.com/prod_pull_n_aws_push.php?repo=frontend
 
