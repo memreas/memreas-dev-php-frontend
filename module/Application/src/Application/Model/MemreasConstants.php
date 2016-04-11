8e7cb96ced17fb007a089b9432cb0cc24bb5dd2a
john meah <johnmeah@memreas.com>
2016-04-11 12:28:27 -0400

    merge fix

--- module/Application/src/Application/Model/MemreasConstants.php
+++ module/Application/src/Application/Model/MemreasConstants.php
@@ -7,10 +7,14 @@
 namespace Application\Model;
 
 class MemreasConstants {
-	const MEMREAS_FE = "https://fe.memreas.com";
-	const MEMREAS_WS = "https://memreasdev-wsj.memreas.com";
-	const MEMREAS_PAY = "https://memreasdev-wsj.memreas.com/index?action=";
-	//const MEMREAS_PAY = "https://memreasdev-pay.memreas.com";
+	
+	// const MEMREAS_FE = "https://fe.memreas.com";
+	// const MEMREAS_WS = "https://memreasdev-wsj.memreas.com";
+	// const MEMREAS_PAY = "https://memreasdev-pay.memreas.com";
+	const MEMREAS_FE = "http://memreas-dev-php-frontend.localhost";
+	const MEMREAS_WS = "http://memreas-dev-php-ws.localhost";
+	const MEMREAS_PAY = "http://memreas-dev-php-ws.localhost/index?action=";
+	//const MEMREAS_PAY = "http://memreas-dev-php-stripe.localhost";
 	const MEMREAS_ADS = false;
 	const MEMREAS_SELL_MEDIA = 1; // Enable or Disable selling media
 	const CLOUDFRONT_DOWNLOAD_HOST = 'https://d3sisat5gdssl6.cloudfront.net/';
@@ -34,6 +38,6 @@ class MemreasConstants {
 	const COPYRIGHT = '&copy;2015 memreas llc';
 	
 	//Android url for download
-	const ANDROID_DOWNLOAD_URL = "https://s3.amazonaws.com/memreas-android-build/memreas-armv7-universal-release.dev.apk";
+	const ANDROID_DOWNLOAD_URL="https://s3.amazonaws.com/memreas-android-build/memreas-armv7-universal-release.dev.apk";
 	
 }
