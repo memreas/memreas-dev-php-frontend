bf90912ee824ff079e8353a95bb76a95165ecf69
john meah <johnmeah@memreas.com>
2016-06-20 14:27:28 -0400

    updated constants and added AES256 constant

--- module/Application/src/Application/Model/MemreasConstants.php
+++ module/Application/src/Application/Model/MemreasConstants.php
@@ -7,24 +7,23 @@
 namespace Application\Model;
 
 class MemreasConstants {
-
-	//Server URLs
-	const MEMREAS_FE = "https://fe.memreas.com";
-	const MEMREAS_WS = "https://memreasdev-wsa.memreas.com";
-	const MEMREAS_WS_PUBLIC = "https://memreasdev-wsa.memreas.com/public";
-	const MEMREAS_PAY = "https://memreasdev-wsa.memreas.com/index?action=";
-	//change..
-	//const MEMREAS_PAY = "https://memreasdev-pay.memreas.com";
-	const MEMREAS_ADS = false;
+	const MEMREAS_FE = "https://www.memreas.com";
+	const MEMREAS_WS = "https://memreasprod-ws.memreas.com";
+	const MEMREAS_WS_PUBLIC = "https://memreasprod-ws.memreas.com/public";
+	const MEMREAS_PAY = "https://memreasprod-ws.memreas.com/index?action=";
+	const MEMREAS_ADS = true;
 	const MEMREAS_SELL_MEDIA = 1; // Enable or Disable selling media
-	const CLOUDFRONT_DOWNLOAD_HOST = 'https://d3sisat5gdssl6.cloudfront.net/';
-	const CLOUDFRONT_HLS_DOWNLOAD_HOST = 'https://d2b3944zpv2o6x.cloudfront.net/';
-	const S3_APPKEY = 'AKIAJZE2O2WDMXLGR27A';
-	const S3_APPSEC = 'FI09T7vRXcWx+QBE/n5ysEtZxx/DOAxkks/o2rzG';
-	const S3BUCKET = "memreasdevsec";
+	const S3_APPKEY = 'AKIAISDIQFVJMWFYXCIA';
+	const S3_APPSEC = 'eM5HG4MbYhkW1Jz1RWIdMapo2s+DbB+KnkhzTt91';
+	const S3_ENCRYPTION = 'AES256';
+	const S3BUCKET = "memreasprodsec";
+	const CLOUDFRONT_DOWNLOAD_HOST = 'https://d321rfaqc9owi4.cloudfront.net/';
+	const CLOUDFRONT_HLS_DOWNLOAD_HOST = 'https://d1fhgtf97i7jlq.cloudfront.net/';
+	const MAINTENANCE = false; //Enable / Disable Maintenance mode
+	
+		
 	const LISTNOTIFICATIONSPOLLTIME = 240000; // 5 minutes
 	const GALLERYDELAYTIME = 500; // 500 ms
-	const MAINTENANCE = false; //Enable / Disable Maintenance mode
 	                              
 	// For file limit upload
 	const FREE_ACCOUNT_FILE_LIMIT = 100; // 100MB limited upload for free user
@@ -37,17 +36,16 @@ class MemreasConstants {
 	const VERSION = '17-JUN-2016';
 	const COPYRIGHT = '&copy; memreas llc';
 	
-	//Android url for download
-	const ANDROID_DOWNLOAD_URL = "https://play.google.com/apps/testing/com.memreas";
-	
 	//Meta
 	const MEMREAS_DESCRIPTION = "memreas is a social media network that allows users to access their content on the web or our Android and iOS apps.  memreas members have the ability to upload or access images or video with 4k support.";
 	const MEMREAS_KEYWORDS = "social networking media streaming 4k HDR";
-	
+
 	// HLS mime types
 	//document.createElement('video').canPlayType('application/vnd.apple.mpegURL')
 	//document.createElement('video').canPlayType('application/x-mpegURL')
 	const HLS_MIME_TYPEX = 'application/x-mpegURL';
 	const HLS_MIME_TYPEV = 'application/vnd.apple.mpegURL';
 	
+
+
 }
