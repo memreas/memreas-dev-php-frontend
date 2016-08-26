5edf52515bd870ae03f3f0f56dbb6578706a02f7
john meah <johnmeah@memreas.com>
2016-08-26 11:24:02 -0400

    added pull script for Shyam to update fe

---
+++ sUpdateFE.sh
@@ -0,0 +1,6 @@
+#Purpose = ssh or sftp into ec2 instance
+#Created on 1-AUG-2016
+#Author = John Meah
+#Version 1.0
+curl https://memreasdev.memreas.com/index?action=clearlog
+curl https://memreasdev.memreas.com/index?action=gitpull
