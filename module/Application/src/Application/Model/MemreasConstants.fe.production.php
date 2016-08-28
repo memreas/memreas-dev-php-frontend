790318e36e21a2609c3c009035511d9fe9355846
john meah <johnmeah@memreas.com>
2016-08-28 17:33:20 -0400

    updated version

--- module/Application/src/Application/Model/MemreasConstants.fe.production.php
+++ module/Application/src/Application/Model/MemreasConstants.fe.production.php
@@ -37,7 +37,7 @@ class MemreasConstants {
 	const PAYMENT_TAB_SUBSCRIPTION = true;
 	const PAYMENT_TAB_BUY_CREDIT = true;
 	const PAYMENT_TAB_SELL_MEDIA = true;
-	const VERSION = '7-JUL-2016';
+	const VERSION = '28-AUG-2016';
 	const COPYRIGHT = '&copy; memreas llc';
 	
 	//Meta
